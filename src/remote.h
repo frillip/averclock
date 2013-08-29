9bfe055d411823273dd9da5cb26395aa50d839fe
Phil Martin <root@frillip.com>
2013-08-29 17:05:59 +0100

    Added disabled AT+ code
    Will adapt for averclock later. Parses strings in a delightfully
    non-blocking way. Recycled from CAN tachometer code.
    
    @naggie

--- src/remote.h
+++ src/remote.h
@@ -55,3 +55,65 @@ void remote_feedback(void)
 	fprintf(COM1,"\r\n%02u:%02u:%02u - %u\r\n",time.hours,time.minutes,time.seconds,light);
 }
 
+uint8_t offset=0;
+char command[16];
+boolean incoming=FALSE;
+unsigned char reset_cmd[] = {"AT+RESET\0"}; 
+
+uint8_t strcmp(unsigned char *s1, unsigned char const *s2)
+{
+   for (; *s1 == *s2; s1++, s2++)
+      if (*s1 == '\0')
+         return(0);
+   return((*s1 < *s2) ? -1: 1);
+}
+
+/*
+#INT_RDA
+void remote_command(void)
+{
+	command[offset]=fgetc(COM1);
+	if(command[offset]==0xff)
+	{
+		delay_ms(10);
+		reset_cpu();
+	}
+	fprintf(COM1,"%c", command[offset]);
+	if(command[offset]==0xd) fprintf(COM1,"\n");
+	if(incoming==FALSE)
+	{
+		if(command[0]=='A')
+		{
+			if((offset)&&(command[1]=='T'))
+			{
+				if ((offset==2)&&(command[2]=='+'))
+				{
+					incoming=TRUE;
+				}
+				else offset=0;
+				offset++;
+			}
+			else offset=0;
+			offset++;
+		}
+		else offset=0;
+	}
+	else offset++;
+	if(((command[offset-1]==0x00)||(command[offset-1]==0xd))&&(incoming))
+	{
+		incoming=FALSE;
+		if(strcmp(command,reset_cmd))
+		{
+			fprintf(COM1, "Resetting...\r\n");
+			delay_ms(10);
+			reset_cpu();
+		}
+	}
+	if(offset==16)
+	{
+		offset=0;
+		fprintf(COM1, "\r\n");
+		incoming=FALSE;
+	}
+}
+*/
